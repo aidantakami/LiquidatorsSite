dedc12bc71a6842576751ac902ace049e6f3b155
Aidan Takami <aidan.takami@yahoo.com>
2020-04-10 14:56:22 -0700

    Added new scene & video player window

---
+++ LiquidatorsSite/Assets/VidepPlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VidepPlayerScript : MonoBehaviour
+{
+
+    public VideoPlayer vp;
+
+    private int missionTracker = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp = gameObject.GetComponent<VideoPlayer>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void TheMissionDown()
+    {
+        if(missionTracker == 0)
+        {
+           // vp.url =;
+        }
+    }
+}
