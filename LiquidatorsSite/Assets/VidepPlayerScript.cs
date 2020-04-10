5494d83374bc15c2fc8e98e8419462edf778f30d
Aidan Takami <aidan.takami@yahoo.com>
2020-04-10 16:35:16 -0700

    Site functionality

--- LiquidatorsSite/Assets/VidepPlayerScript.cs
+++ LiquidatorsSite/Assets/VidepPlayerScript.cs
@@ -6,7 +6,12 @@ using UnityEngine.Video;
 public class VidepPlayerScript : MonoBehaviour
 {
 
-    public VideoPlayer vp;
+    private VideoPlayer vp;
+
+    private string missionVid1 = "https://dl.dropboxusercontent.com/s/m670bb88tjsiww7/TheMission_P1_v1.mp4?dl=1";
+    private string missionVid2 = "https://dl.dropboxusercontent.com/s/m670bb88tjsiww7/TheMission_P1_v1.mp4?dl=1";
+    private string missionVid3 = "https://dl.dropbox.com/s/d6vkzzzpibyyej3/TheMission_P3_v1.mp4?dl=1";
+
 
     private int missionTracker = 0;
 
@@ -25,7 +30,15 @@ public class VidepPlayerScript : MonoBehaviour
     {
         if(missionTracker == 0)
         {
-           // vp.url =;
+           vp.url = missionVid2;
+            missionTracker++;
+        }
+        else if(missionTracker == 1)
+        {
+            //darken button
+            vp.url = missionVid3;
+            missionTracker++;
+
         }
     }
 }
