faa138686dd3fc31407f8af1096b976592dc3647
Aidan Takami <aidan.takami@yahoo.com>
2020-05-10 22:14:26 -0700

    Added more video buttons

--- LiquidatorsSite/Assets/VidepPlayerScript.cs
+++ LiquidatorsSite/Assets/VidepPlayerScript.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -8,12 +7,20 @@ public class VidepPlayerScript : MonoBehaviour
 
     private VideoPlayer vp;
 
-    private string missionVid1 = "https://dl.dropboxusercontent.com/s/m670bb88tjsiww7/TheMission_P1_v1.mp4?dl=1";
-    private string missionVid2 = "https://dl.dropboxusercontent.com/s/m670bb88tjsiww7/TheMission_P1_v1.mp4?dl=1";
-    private string missionVid3 = "https://dl.dropbox.com/s/d6vkzzzpibyyej3/TheMission_P3_v1.mp4?dl=1";
+    private string crewVid1 = "https://dl.dropbox.com/s/68addszz2wrf1w7/F_TheCrew_P1_small.mp4?dl=1";
+    private string crewVid2 = "https://dl.dropbox.com/s/tnoz3mtklem7137/F_TheCrew_P2_small.mp4?dl=1";
+
+    private string missionVid1 = "https://dl.dropbox.com/s/ctvciybnior9bh3/F_TheMission_P1_small.mp4?dl=1";
+    private string missionVid2 = "https://dl.dropbox.com/s/h8i8ly3wg1b65yn/F_TheMission_P2_small.mp4?dl=1";
+    private string missionVid3 = "https://dl.dropbox.com/s/784qlv0u3ddrc22/F_TheMission_P3_small.mp4?dl=1";
 
 
-    private int missionTracker = 0;
+    private int scrollProgress = 0;
+
+    //Home screen = 0
+    //The Crew = 1
+    // 
+    private int currentlyActivePage = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,22 +30,111 @@ public class VidepPlayerScript : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ScrollDown();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ScrollUp();
+        }
     }
 
-    public void TheMissionDown()
+    public void TheCrewPressed()
     {
-        if(missionTracker == 0)
-        {
-           vp.url = missionVid2;
-            missionTracker++;
-        }
-        else if(missionTracker == 1)
-        {
-            //darken button
-            vp.url = missionVid3;
-            missionTracker++;
+        vp.url = crewVid1;
+        ResetPageVariables();
+        currentlyActivePage = 1;
+    }
 
+    public void TheMissionPressed()
+    {
+        vp.url = crewVid1;
+        ResetPageVariables();
+        currentlyActivePage = 2;
+    }
+
+    public void TheCrewScroll()
+    {
+        if(scrollProgress == 0)
+        {
+           vp.url = crewVid1;
         }
+        else if(scrollProgress == 1)
+        {
+            vp.url = crewVid2;
+        }
+        else if(scrollProgress == 2)
+        {
+            scrollProgress--;
+        }
+        else if(scrollProgress == -1)
+        {
+            scrollProgress++;
+        }
+    }
+
+    private void TheMissionScroll()
+    {
+        if (scrollProgress == 0)
+        {
+            vp.url = missionVid1;
+        }
+        else if (scrollProgress == 1)
+        {
+            vp.url = missionVid2;
+        }
+        else if (scrollProgress == 2)
+        {
+            vp.url = missionVid3;
+        }
+        else if (scrollProgress == 3)
+        {
+            scrollProgress--;
+        }
+        else if(scrollProgress == -1)
+        {
+            scrollProgress++;
+        }
+    }
+
+    public void ScrollDown()
+    {
+        scrollProgress++;
+
+
+        if(currentlyActivePage == 1)
+        {
+            TheCrewScroll();
+        }
+        else if(currentlyActivePage == 2)
+        {
+            TheMissionScroll();
+        }
+    }
+
+
+    public void ScrollUp()
+    {
+        scrollProgress--;
+
+
+        if (currentlyActivePage == 1)
+        {
+            TheCrewScroll();
+        }
+        else if (currentlyActivePage == 2)
+        {
+            TheMissionScroll();
+        }
+
+
+    }
+
+
+    private void ResetPageVariables()
+    {
+        scrollProgress = 0;
     }
 }
